918c6c04e8376491d4879b593b8e75d61c32ac77
sudarshank264 <sudarshank264@gmail.com>
2026-04-08 05:23:34 -0700

    new commit

--- backend/routes/reviewRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getReviews, createReview } = require('../controllers/reviewController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', getReviews);
-router.post('/', protect, createReview); // Admin token required to post a review
-
-module.exports = router;
